120122ecad97ae2eda3e3700b713d5aa36cac6ea
Elyse <elyse@redcurrent.com>
2017-08-08 13:59:47 -0400

    added heroku

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 var routes = require('./routes');
 
-app.use('/languages', routes);
+app.use('/', routes);
 app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(8080, function () {
-  var port = server.address().port;
+var port = process.env.PORT || 5000;
+app.listen(port, function () {
   console.log('App\'s server listening at http://localhost:%s', port);
 });
