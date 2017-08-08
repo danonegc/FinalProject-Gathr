3025b7193c0f65e223edaad8d4449713c9f6ec7a
Elyse <elyse@redcurrent.com>
2017-08-08 14:35:55 -0400

    merge conflicts

--- public/js/directives/partyDetails-directive.js
+++ public/js/directives/partyDetails-directive.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-// var app = angular.module('gathrApp');
-//
-// app.directive('', function() {
-//   return {
-//     restrict: 'E',
-//     replace: false,
-//     templateUrl: ""
-//   }
-// });
-=======
 var app = angular.module('gathrApp');
 
 app.directive('partyDetails', function() {
@@ -19,4 +8,3 @@ app.directive('partyDetails', function() {
     // template: "<div>{{ data.partyName }}</div>"
   };
 });
->>>>>>> 0da900437fcaa415ab30198b7748987bc80663ae
