fbd0d8640f02e436e6334fe5de5a4fcd0d706996
Elyse <elyse@redcurrent.com>
2017-08-08 22:42:08 -0400

    added Items by Category Icons, buttons, and functionality to display on click

--- routes.js
+++ routes.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var stuff = require('./data');
 var pool = require('./pg-connection-pool');
 
-router.get('/', function (req, res) {
-  res.json(stuff[Math.floor(Math.random() * stuff.length)]);
-});
+// router.get('/', function (req, res) {
+//   res.json(stuff[Math.floor(Math.random() * stuff.length)]);
+// });
 
 module.exports = router;
