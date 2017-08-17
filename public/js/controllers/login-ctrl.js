c10f76bf5eda5d65a1a84043ad732702679b37e4
Reid Trierweiler <RT.Trierweiler@gmail.com>
2017-08-17 13:03:34 -0400

    login page

--- public/js/controllers/login-ctrl.js
+++ public/js/controllers/login-ctrl.js
@@ -1,10 +1,17 @@
 var app = angular.module('gathrApp');
 
-app.controller('login-ctrl', function($scope, $location, gathrFactory) {
+app.controller('login-ctrl', function($scope, $location, gathrFactory, $interval) {
 
 $scope.submitLogin = function(userInfo) {
     gathrFactory.checkLogin(userInfo);
   };
+
+
+  $scope.logoTransition = true;
+$interval(function(){
+  $scope.logoTransition = false
+}, 1000);
+
 
 
 //closure for app.controller
--- public/js/module/module.js
+++ public/js/module/module.js
@@ -1,1 +1,1 @@
-var app = angular.module('gathrApp', ['ngRoute']);
+var app = angular.module('gathrApp', ['ngRoute', 'ngAnimate']);
